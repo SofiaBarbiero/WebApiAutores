d42b89766fa083af9d28e439c2425a431302ca4a
SofiaBarbiero <sofibarbiero@gmail.com>
2023-11-16 12:18:06 -0300

    settings

---
+++ Middlewares/LoguearRespuestaHttpMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace WebAppAutores.Middlewares
+{
+    public class LoguearRespuestaHttpMiddleware
+    {
+        private readonly RequestDelegate siguiente;
+        private readonly ILogger<LoguearRespuestaHttpMiddleware> logger;
+
+        public LoguearRespuestaHttpMiddleware(RequestDelegate siguiente, 
+            ILogger<LoguearRespuestaHttpMiddleware> logger)
+        {
+            this.siguiente = siguiente;
+            this.logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext contexto)
+        {
+            using(var ms = new MemoryStream())
+            {
+                var cuerpoOriginalRta = contexto.Response.Body;
+                contexto.Response.Body = ms;
+
+                await siguiente(contexto);
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -18,7 +18,7 @@ namespace WebAppAutores
         
 
 
-        public void ConfigureServices(IServiceCollection services) //para configurar mis servicios
+        public void ConfigureServices(IServiceCollection services) //para configurar mis servicios, injecciones de deependencia
         {
             services.AddControllers().AddJsonOptions(x =>
                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); //configuracion para cortar ciclos infinitos en las relaciones de entidades
@@ -33,15 +33,18 @@ namespace WebAppAutores
             });
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) //configurar mis middleware
         {
             // Configure the HTTP request pipeline.
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAppAutores v1"));
+                //si dejo el .UseSwagger() - .UseSwaggerUI() aca, solo tengo swagger en ambiente de desarrollo
             }
+
+            //los coloco aca para tener swagger en ambiente de produccion
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAppAutores v1"));
 
             app.UseHttpsRedirection();
 
